c23709d708cf7e7dfd842369300fe17e056c3058
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-08-31 00:04:55 +0300

    Number 1

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,9 +11,10 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
+
 	if (newNode == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 
 	newNode->n = value;
@@ -27,5 +28,5 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	{
 		newNode->parent = NULL;
 	}
-	return newNode;
+	return (newNode);
 }
