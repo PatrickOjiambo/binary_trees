c8ae446352be5a0654b998970e209beb2f4be40a
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-08-31 12:48:32 +0300

    Update 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -6,7 +6,7 @@
  * @value: value to store in the new node
  *
  * Return: pointer to the created node, or NULL on failure
- * or if parent is NULL
+ *  or if parent is NULL
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
@@ -20,6 +20,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	newNode->left = NULL;
 	newNode->right = NULL;
 	newNode->parent = parent;
+	if (parent->right != NULL)
 	{
 		newNode->right = parent->right;
 		parent->right->parent = newNode;
