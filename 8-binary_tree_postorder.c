bea6e87022faf2fa442a2930acb71fceb9a7dbe6
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-08-31 13:25:11 +0300

    Updated all

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -13,8 +13,8 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	{
 		return;
 	}
-	binary_tree_postorder(tree->right, &print_num);
+	binary_tree_postorder(tree->right, func);
 	func(tree->n);
-	binary_tree_postorder(tree->left, &print_num);
+	binary_tree_postorder(tree->left, func);
 
 }
